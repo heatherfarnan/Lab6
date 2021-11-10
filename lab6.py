0d6fbcc1040a5a89c0ddb4f2dafffd2a9efa2e46
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-11-10 23:55:34 +0000

    cup stuff

--- lab6.py
+++ lab6.py
@@ -2,8 +2,8 @@ from led8x8 import LED8x8
 
 dataPin, latchPin, clockPin = 23, 24, 25
 
-#8x8 = LED8x8(dataPin, latchPin, clockPin)
+led = LED8x8(dataPin, latchPin, clockPin)
 #yuh
 
 while True:
-  LED8x8.self.display()
+  led.display()
