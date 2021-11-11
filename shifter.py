c701af04d51762015dafde8ac1880bb86346b552
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-11-11 22:50:45 +0000

    try again with the latch method now that common cathode

--- shifter.py
+++ shifter.py
@@ -24,8 +24,8 @@ class Shifter():
       #GPIO.output(self.dataPin, ~(byteVal & (1<<i)))  # if common anode
       GPIO.output(self.dataPin, byteVal & (1<<i))    # if common cathode
       self.ping(self.clockPin)
-    self.ping(self.latchPin)
-
-#  def latch(self):
 #    self.ping(self.latchPin)
+
+  def latch(self):
+    self.ping(self.latchPin)
 
