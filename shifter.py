6f4016e838236074107bb57cf89efbf7636441e5
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-11-11 22:18:22 +0000

    got rid of latch ping at the end of shiftByte

--- shifter.py
+++ shifter.py
@@ -24,7 +24,7 @@ class Shifter():
       GPIO.output(self.dataPin, ~(byteVal & (1<<i)))  # if common anode
       #GPIO.output(self.dataPin, byteVal & (1<<i))    # if common cathode
       self.ping(self.clockPin)
-    self.ping(self.latchPin)
+#    self.ping(self.latchPin)
 
   def latch(self):
     self.ping(self.latchPin)
