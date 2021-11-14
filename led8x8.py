2f49d452c92ab8fe0302d3d38326ee267a20e2b1
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-11-14 22:41:44 +0000

    changed column trigger numbers

--- led8x8.py
+++ led8x8.py
@@ -55,13 +55,13 @@ class LED8x8():
         LED8x8.i += 1
     c = random.randint(0,2)
     if c == 0:
-      if a[LED8x8.i] < 256:
+      if a[LED8x8.i] < 127:
         a[LED8x8.i] << 1
         a[LED8x8.i] += 1
     if c == 1:
       pass
     if c == 2:
-      if a[LED8x8.i] < 510:
+      if a[LED8x8.i] < 254:
         a[LED8x8.i] >> 1
         a[LED8x8.i] += 256
     time.sleep(.1)
