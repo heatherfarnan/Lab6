a0d6d7d890e33e4ac339f88a6e50b5bcc5fb1dc8
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-11-14 21:11:42 +0000

    changed column logic to try to make not the full row on

--- led8x8.py
+++ led8x8.py
@@ -50,12 +50,14 @@ class LED8x8():
         LED8x8.i += 1
     c = random.randint(0,2)
     if c == 0:
-      a[LED8x8.i] << 1
-      if a[LED8x8.i] == 0b11111111:
-        a[LED8x8.i] = 0b01111111
+      if a[LED8x8.i] < 256:
+        a[LED8x8.i] << 1
+        a[LED8x8.i] += 1
+
     if c == 1:
       pass
     if c == 2:
-      a[LED8x8.i] >> 1
-      if a[LED8x8.i] == 0b11111111:
-        a[LED8x8.i] = 0b11111110
+      if a[LED8x8.i] < 510:
+        a[LED8x8.i] >> 1
+        a[LED8x8.i] += 256
+        
